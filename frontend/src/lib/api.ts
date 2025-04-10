e7be006050ae575c251c10dd6edb3a13c610f0c7
sourav4243 <souravkumar18835@gmail.com>
2025-04-10 22:56:54 +0530

    added backend api service from render to api.ts and added env variables to .env

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,4 +1,6 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000/api';
+
 
 interface ApiResponse<T> {
   data: T;
